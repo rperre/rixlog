fd522734fb5d00d16e40ffb08375b057420757d0
Ricardo Perre <rjmperre@gmail.com>
2024-08-28 12:50:56 +0100

    using Chi to render

--- internal/controllers/articles.go
+++ internal/controllers/articles.go
@@ -2,14 +2,21 @@ package controllers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
 	"net/http"
 	"rixlog/internal/models"
+
+	"github.com/go-chi/chi/v5"
+
+	"github.com/go-chi/chi/v5"
+
+	"github.com/go-chi/chi/v5"
 	"rixlog/internal/views"
 	"strconv"
 	"strings"
-
-	"github.com/go-chi/chi/v5"
 )
 
 func Articles() *ArticlesController {
@@ -34,6 +41,34 @@ func (a *ArticlesController) Routes() chi.Router {
 	return r
 }
 
+func (a *ArticlesController) ExistingArticle(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		model := models.Article()
+		id, _ := strconv.ParseInt(chi.URLParam(r, "id"), 10, 64)
+		fmt.Println(chi.URLParam(r, "id"))
+		fmt.Println(id)
+		if article, err := model.GetByID(id); err != nil {
+			HttpError(err, http.StatusNotFound, w, r)
+			return
+		} else {
+			ctx := context.WithValue(r.Context(), "article", article)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		}
+	})
+}
+
+type ArticleResponse struct {
+	*models.ArticleJSON
+
+	Elapsed int64 `json:"elapsed"`
+}
+
+func (rd *ArticleResponse) Render(w http.ResponseWriter, r *http.Request) error {
+	// Pre-processing before a response is marshalled and sent across the wire
+	rd.Elapsed = 0
+	return nil
+}
+
 func (a *ArticlesController) GetArticle(w http.ResponseWriter, r *http.Request) {
 	model := models.Article()
 	id, _ := strconv.ParseInt(r.URL.Query().Get("id"), 10, 64)
@@ -45,6 +80,8 @@ func (a *ArticlesController) GetArticle(w http.ResponseWriter, r *http.Request) 
 			views.Article(w, r, article)
 		} else if strings.Contains(accpet_header, "application/json") {
 			marsh, _ := json.Marshal(article)
+			render.Status(r, http.StatusOK)
+			_ = render.Render(w, r, &ArticleResponse{ArticleJSON: article})
 			_, _ = w.Write(marsh)
 		}
 	}
@@ -52,7 +89,7 @@ func (a *ArticlesController) GetArticle(w http.ResponseWriter, r *http.Request) 
 
 func (a *ArticlesController) Void(w http.ResponseWriter, r *http.Request) {}
 
-func HtppRerror(err error, w http.ResponseWriter, r *http.Request) {
+func HttpError(err error, code int, w http.ResponseWriter, r *http.Request) {
 	resp := make(map[string]string)
 	resp["message"] = err.Error()
 	marsh, _ := json.Marshal(resp)
